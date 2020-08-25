905f3fa86ec6eaae767c1d930c2dcbde1d4d2ad7
codevictory <main@codevictory.net>
2020-08-25 16:24:38 +0300

    First test.

---
+++ Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Model;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pharaoh asa = new Pharaoh
+            {
+                Name = "sadsad"
+            };
+
+            System.Console.WriteLine(asa.Name);
+        }
+    }
+}
