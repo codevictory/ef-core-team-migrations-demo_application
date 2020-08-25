0cd5264a4dcf8272f7369ea167797c2f9b0d5248
codevictory <main@codevictory.net>
2020-08-25 15:54:45 +0300

    Initial project skeleton.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ef_core_team_migrations_demo_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
